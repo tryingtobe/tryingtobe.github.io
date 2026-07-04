f5a2ffe588159f2b06a09bc13ec434563c1a1c16
Aiym <aiym@enquirytracker.net>
2026-07-04 13:09:52 +0600

    Add Bluebound Ink site: Astro setup, components, pages, config

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Bluebound Ink';
+export const SITE_DESCRIPTION = 'A blog by tryingtobe';
